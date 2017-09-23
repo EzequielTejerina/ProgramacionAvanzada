17b76885d84a8286cb55bd5582254fa23f8b3712
Ezequiel Luis Tejerina <31083999+EzequielTejerina@users.noreply.github.com>
2017-09-23 20:50:10 -0300

    Delete Estado.java

--- PiratasSaludables/src/piratassaludables/Estado.java
+++
@@ -1,9 +0,0 @@
-package piratassaludables;
-
-public abstract class Estado {
-	public abstract Estado beberGrog();
-	public abstract Estado beberJugo();
-	public abstract int atacar();
-	public abstract int defender();
-	
-}
